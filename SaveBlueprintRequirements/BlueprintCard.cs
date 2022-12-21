2e3bb144e914a1cd025feaffabf7582eb863fa95
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-12-21 14:34:46 -0500

    Converted to JSON

---
+++ SaveBlueprintRequirements/BlueprintCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveBlueprintRequirements
+{
+    public class BlueprintCard
+    {
+        public string Name { get; set; }
+        public List<BlueprintResource> Resources { get; set; } = new List<BlueprintResource>();
+    }
+}
